06b7d3c98fbc4724552b0c6f4ecba606a42ece04
Babar Afzaal <babarmalik6444@gmail.com>
2023-07-31 20:51:54 +0200

    Improved bot checker using Redis cache

--- config.php
+++ config.php
@@ -6,4 +6,7 @@ return [
     'incoming_channel' => 'visitor-analytics',
     'outgoing_channel' => 'visitor-analytics-2',
     'local_json_file' => __DIR__ . '/../conversions.json',
+    'botAddresses' => 'https://antoinevastel.com/data/avastel-longtime-bot-ips.txt',
+    "redis_cache_expiry" =>  3600, //seconds
+    "redis_cache_key" =>  'known_bot_addresses'
 ];
--- src/BotChecker/DefaultBotChecker.php
+++ src/BotChecker/DefaultBotChecker.php
@@ -2,13 +2,55 @@
 
 namespace App\BotChecker;
 
+use Predis\Client;
+
 class DefaultBotChecker implements BotCheckerInterface
 {
+    private $redis;
+
+    public function __construct()
+    {
+        $this->redis = new Client([
+            'host' => getConfig('redis_host'),
+            'port' => getConfig('redis_port')
+        ]);
+    }
+
     public function isKnownBotAddress(string $ip): bool
     {
-        $botAddressesUrl = 'https://antoinevastel.com/data/avastel-longtime-bot-ips.txt';
-        $knownBotAddresses = file($botAddressesUrl, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $cachedKnownBotAddresses = $this->getFromCache(getConfig('redis_cache_key'));
+
+        if ($cachedKnownBotAddresses === null) {
+            $knownBotAddresses = $this->fetchKnownBotAddresses();
+            $this->storeInCache(getConfig('redis_cache_key'), $knownBotAddresses, getConfig('redis_cache_expiry'));
+        } else {
+            $knownBotAddresses = json_decode($cachedKnownBotAddresses, true);
+        }
 
         return in_array($ip, $knownBotAddresses);
+    }
+
+    private function fetchKnownBotAddresses(): array
+    {
+        $botAddressesUrl = getConfig('botAddresses');
+        $lines = file($botAddressesUrl, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) ?: [];
+
+        $ipAddresses = [];
+        foreach ($lines as $line) {
+            $data = explode(";", $line);
+            $ipAddresses[] = $data[0];
+        }
+
+        return $ipAddresses;
+    }
+
+    private function getFromCache(string $key)
+    {
+        return $this->redis->get($key);
+    }
+
+    private function storeInCache(string $key, $data, int $ttl)
+    {
+        $this->redis->setex($key, $ttl, json_encode($data));
     }
 }
